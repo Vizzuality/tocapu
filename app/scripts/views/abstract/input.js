8f9b1b8f0d796c20b3dc5e0d2a1e12385350965d
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2015-05-20 17:56:57 +0200

    Adds the select view and moves the newly created views to abstract

---
+++ app/scripts/views/abstract/input.js
@@ -0,0 +1,50 @@
+/*jshint bitwise:false*/
+define([
+  'underscore',
+  'backbone',
+  'views/abstract/base'
+], function(_, Backbone, BaseView) {
+
+  'use strict';
+
+  var InputView = BaseView.extend({
+
+    template: '<input value="{{{value}}}" />',
+
+    _model: new (Backbone.Model.extend())(),
+
+    /** Used for the validation of the model */
+    validate: function() {},
+
+    serialize: function() {
+      if(this._model.isValid()) {
+        return {
+          value: this._model.get('value')
+        };
+      }
+      return {
+        value: this._model.get('value'),
+        error: this._model.validationError
+      };
+    },
+
+    initialize: function() {
+      this._model.validate = this.validate;
+      this._model.on('change', this.render);
+    },
+
+    get: function() {
+      return this._model.get('value');
+    },
+
+    set: function(value) {
+      this._model.set({ value: value });
+      this.render();
+      return this._model.get('value');
+    }
+
+  });
+
+  return InputView;
+
+});
---
+++ app/scripts/views/abstract/select.js
@@ -0,0 +1,54 @@
+/*jshint bitwise:false*/
+define([
+  'underscore',
+  'backbone',
+  'views/abstract/input'
+], function(_, Backbone, InputView) {
+
+  'use strict';
+
+  var SelectView = InputView.extend({
+
+    template: '<select>' +
+              '{{#each options}}' +
+              '<option>{{{value}}}</option>' +
+              '{{/each}}' +
+              '</select>',
+
+    /* Stores the options */
+    _collection: new (Backbone.Collection.extend({}))(),
+
+    /* Override */
+    serialize: function() {
+      if(this.model.isValid()) {
+        return {
+          options: _.pick(this.model.get('collection').toJSON(), 'options'),
+          value: this.model.get('value')
+        };
+      }
+      return {
+        options: _.pick(this.model.get('collection').toJSON(), 'options'),
+        value: this.model.get('value'),
+        error: this.model.validationError
+      };
+    },
+
+    /* Override */
+    initialize: function() {
+      this.model.set({ collection: this._collection });
+      this.model.validate = this.validate;
+      this.model.on('change', this.render);
+      this._collection.on('change', this.setCollection);
+    },
+
+    /* Useful for asynchronous changes */
+    setCollection: function(collection) {
+      this.model.set({ collection: collection });
+      return this.model.get('collection');
+    }
+
+  });
+
+  return SelectView;
+
+});
